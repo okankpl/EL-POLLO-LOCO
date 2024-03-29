630a02e3d85bd9816c9067b8db1cc66afb18dcbc
Okan Kaplan <okan149@gmail.com>
2024-03-29 14:44:14 +0100

    adding keyboard event

--- js/game.js
+++ js/game.js
@@ -1,12 +1,13 @@
 let canvas;
 let ctx;
-
-
 let world;
 
 function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas);
-
     console.log('My Character is,', world.character);
 }
+
+window.addEventListener('keydown', (e) => {
+    console.log(e);
+});
