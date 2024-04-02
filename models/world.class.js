2c0da443426eb4930930f4fa466b41107c008888
Okan Kaplan <okan149@gmail.com>
2024-04-02 12:37:53 +0200

    added background layer

--- models/world.class.js
+++ models/world.class.js
@@ -4,10 +4,25 @@ class World {
 
   clouds = [new Cloud()];
   backgroundObject = [
+    new BackgroundObject("img/5_background/layers/air.png", -719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719, 480),
+
     new BackgroundObject("img/5_background/layers/air.png", 0),
     new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
     new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0, 480),
+
+    new BackgroundObject("img/5_background/layers/air.png", 719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719, 480),
+
+    new BackgroundObject("img/5_background/layers/air.png", 2*719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 2*719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 2*719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 2*719, 480),
   ];
 
   canvas;
