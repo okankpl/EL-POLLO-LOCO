bd6ce8959f9f35169dd0af2bd9d29da6b703a604
Okan Kaplan <okan149@gmail.com>
2024-04-12 14:46:58 +0200

    adding collision func to enemies

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -70,12 +70,19 @@ class MovableObject {
     this.x -= this.speed;
   }
 
-  isColliding (obj) {
-    return  (this.x + this.width) >= obj.x && this.x <= (obj.x + obj.width) && 
-            (this.y + this.offsetY + this.height) >= obj.y &&
-            (this.y + this.offsetY) <= (obj.y + obj.height) && 
-            obj.onCollisionCourse; // Optional: hiermit könnten wir schauen, ob ein Objekt sich in die richtige Richtung bewegt. Nur dann kollidieren wir. Nützlich bei Gegenständen, auf denen man stehen kann.
+   isColliding(mo) {
+    // return (
+    //   this.x + this.width >= obj.x &&
+    //   this.x <= obj.x + obj.width &&
+    //   this.y + this.offsetY + this.height >= obj.y &&
+    //   this.y + this.offsetY <= obj.y + obj.height &&
+    //   obj.onCollisionCourse
+    // ); // Optional: hiermit könnten wir schauen, ob ein Objekt sich in die richtige Richtung bewegt. Nur dann kollidieren wir. Nützlich bei Gegenständen, auf denen man stehen kann.
   
+    return this.x + this.width > mo.x &&
+    this.y + this.height > mo.y &&
+    this.x < mo.x &&
+    this.y <mo.y + mo.height
   }
 }
 
--- models/world.class.js
+++ models/world.class.js
@@ -12,6 +12,21 @@ class World {
     this.canvas = canvas;
     this.draw();
     this.setWorld(keyboard);
+    this.checkCollisions();
+  }
+
+  setWorld() {
+    this.character.world = this;
+  }
+
+  checkCollisions() {
+    setInterval(() => {
+      this.level.enemies.forEach((enemy) => {
+        if (this.character.isColliding(enemy)) {
+          console.log('Collision with Character,', enemy);
+        }
+      });
+    }, 1000);
   }
 
   draw() {
@@ -47,10 +62,6 @@ class World {
     if (mo.otherDirection) {
       this.flipImageBack(mo);
     }
-  }
-
-  setWorld() {
-    this.character.world = this;
   }
 
   flipImage(mo) {
