dbf4abc9fcfb6920c2d986cfddcb2aa057e7abb7
Okan Kaplan <okan149@gmail.com>
2024-04-15 20:37:58 +0200

    added resolveimageindex

--- models/statusbar.class.js
+++ models/statusbar.class.js
@@ -1,20 +1,38 @@
-class Statusbar extends DrawableObject{
-    IMAGES = [
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png',
-    ];
+class Statusbar extends DrawableObject {
+  IMAGES = [
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png",
+  ];
 
-    percentage = 100;
+  percentage = 100;
 
-    constructor() {
-        this.loadImages(this.IMAGES);
+  constructor() {
+    this.loadImages(this.IMAGES);
+  }
+
+  setPercentage(percentage) {
+    this.percentage = percentage;
+    let imagePath = this.IMAGES[this.resolveImageIndex()];
+    this.img = this.imageCache[imagePath];
+  }
+
+  resolveImageIndex() {
+    if (this.percentage == 100) {
+      return 5;
+    } else if (this.percentage > 80) {
+      return 4;
+    } else if (this.percentage > 60) {
+      return 3;
+    } else if (this.percentage > 40) {
+      return 2;
+    } else if (this.percentage > 20) {
+      return 1;
+    } else {
+      return 0;
     }
-
-    setPercentage(percentage) {
-        this.percentage = percentage;
-    }
+  }
 }
